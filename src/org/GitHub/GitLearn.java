3f3d941ebf8ba1f6914b27db277fd71987b55c93
Arunyuva <arunyuva9313@gmail.com>
2022-01-31 19:05:08 +0530

    class room timing

--- src/org/GitHub/GitLearn.java
+++ src/org/GitHub/GitLearn.java
@@ -1,5 +1,12 @@
 package org.GitHub;
 
 public class GitLearn {
-
+	
+	private void room() {
+System.out.println("the class name is PUGAL");
+	}
+	
+	private void time() {
+	System.out.println("the class timing is 8:30");
+}
 }
